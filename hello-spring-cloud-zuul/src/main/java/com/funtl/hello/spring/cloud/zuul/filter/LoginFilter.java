808828827e9c62d2ed7c84e091dafec9a3c0072c
xk666 <40770795@qq.com>
2019-05-08 01:13:07 +0800

    添加了Zuul的服务过滤功能

---
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/filter/LoginFilter.java
@@ -0,0 +1,66 @@
+package com.funtl.hello.spring.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Zuul 的服务过滤演示
+ *
+ * @author hk
+ * @version 1.0.0
+ * @date 2019/5/8
+ */
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    /**
+     * 配置过滤类型，有四种不同生命周期的过滤器类型
+     * 1. pre：路由之前
+     * 2. routing：路由之时
+     * 3. post：路由之后
+     * 4. error：发送错误调用
+     * @return
+     */
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤的具体业务代码
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String token = request.getParameter("token");
+        if (token == null) {
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("Token is empty!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
